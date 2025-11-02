2037efff2401c26a65bf685fd42a49856c20221b
Javier <fjrobles@gmail.com>
2025-11-02 23:42:58 +0100

    Update U2EJ1.js
    Ejercicio U2EJ1

--- U2/U2EJ1.js
+++ U2/U2EJ1.js
@@ -31,7 +31,11 @@ for(let i=0; i<10;i++){
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
 
-
+function fibonacci(n){
+	if(n===0){return 0;}
+	if(n===1){return 1;}
+	return fibonacci(n-1)+fibonacci(n-2);
+}
 
 
 /**
