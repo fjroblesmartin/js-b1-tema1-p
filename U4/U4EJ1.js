6fcbbb719c0a2d8c50631f16c9e0f36dd443b700
Javier <fjrobles@gmail.com>
2025-11-04 22:25:09 +0100

    Update U4EJ1.js
    Hecho

--- U4/U4EJ1.js
+++ U4/U4EJ1.js
@@ -16,7 +16,9 @@ console.log(reverseArray(4,7,21,3)); //This will return [3,21,7,4]
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
 
-
+function reverseArray(...numeros){
+  return numeros.reverse();
+}
 
 
 /**
