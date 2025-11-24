96524e695dacd8312de9f2628d20fcb829cd1887
Fco. Javier R. M. <fjrobles@gmail.com>
2025-11-24 10:14:37 +0100

    Update U5EJ3.js

--- U5/U5EJ3.js
+++ U5/U5EJ3.js
@@ -7,13 +7,7 @@
   del sistema solar, se nos pide, mediante una función de flecha que definiremos en la constante ya dada [planetHasRings],
   y utilizando el método filter , definir en la constante [result], un nuevo Array en el cual solamente tendremos planetas con anillos 
   (los definidos por la propiedad hasRings = true)
-	
-	-- CATALÀ --
-	EXERCICI 1.5.3 ENUNCIAT:
-    Anàlogament a l'exercici 1.1.2, donat l'Array [planets], compost d'objectes literals de JS que representen els planetes
-  del sistema solar, se'ns demana, mitjançant una funció de fletxa que definirem a la constant ja donada [planetHasRings],
-  i utilitzant el mètode filter , definir en la constant [result], un nou Array en el qual només tindrem planetes amb anells
-  (els definits per la propietat hasRings = true)
+
 */
 let planets = [
         {
@@ -70,7 +64,6 @@ let planets = [
 const planetHasRings = (planeta)=> planeta.hasRings;
     
 const result= planets.filter(planetHasRings);
-
 
 /**
  * TEST
